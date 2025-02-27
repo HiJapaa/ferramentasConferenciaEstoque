989bfc16c89862968c574ba87528f13ce966ef1c
HiJapaa <pkenohacker@hotmail.com>
2025-02-27 09:33:29 -0400

    update env

--- src/services/firebaseConnection.jsx
+++ src/services/firebaseConnection.jsx
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 
-const apiKey = process.env.REACT_APP_apiKey
-const authDomain = process.env.REACT_APP_authDomain
-const projectId = process.env.REACT_APP_projectId
-const storageBucket = process.env.REACT_APP_storageBucket
-const messagingSenderId = process.env.REACT_APP_messagingSenderId
-const appId = process.env.REACT_APP_appId
+const apiKey = import.meta.env.VITE_apiKey
+const authDomain = import.meta.env.VITE_authDomain
+const projectId = import.meta.env.VITE_projectId
+const storageBucket = import.meta.env.VITE_storageBucket
+const messagingSenderId = import.meta.env.VITE_messagingSenderId
+const appId = import.meta.env.VITE_appId
 
 const firebaseConfig = {
     apiKey: apiKey,
